47be1d7f98a7d9e697998a041776f2905069452d
Jamie Anne White <jamie.anne.white@gmail.com>
2020-08-09 12:09:49 -0500

    working to connect to servers still with pool.js

--- server/public/server.js
+++ server/public/server.js
@@ -1,14 +1,24 @@
 // Require express
 const express = require('express');
-
-// call express
 const app = express();
-const port = 5000;
+const bodyParser = require('body-parser');
+//#red 
+// const koalaRouter = require('./routes/koala.router')
+//#
+const PORT = 5000;
+//const toDoTRouter = require('./routes/koala.router')
+
 
 // express static file serving - public is the folder name
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
+//ROUTES
+//#red 
+// app.use('/koalas', koalaRouter)
+//#
+
 // Start up our server
-app.listen(port, () => {
-  console.log('listening on port', port);
+app.listen(PORT, () => {
+  console.log('listening on PORT', PORT);
 });
