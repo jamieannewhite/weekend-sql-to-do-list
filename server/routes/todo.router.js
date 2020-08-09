5ee57976139c2f9423252c7d61f469b05dadacba
Jamie Anne White <jamie.anne.white@gmail.com>
2020-08-09 13:15:53 -0500

    connected to server, need to make the requests

---
+++ server/routes/todo.router.js
@@ -0,0 +1,4 @@
+const express = require('express');
+// const { Router } = require('express');
+const toDoRouter = express.Router();
+const pool = require('../modules/pool');
--- server/server.js
+++ server/server.js
@@ -2,11 +2,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-//#red 
-// const koalaRouter = require('./routes/koala.router')
-//#
+const toDoRoute = require('./routes/todo.router')
+
 const PORT = 5000;
-//const toDoTRouter = require('./routes/koala.router')
+// const toDoRouter = require('./routes/todo.router')
 
 
 // express static file serving - public is the folder name
@@ -14,9 +13,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
 //ROUTES
-//#red 
-// app.use('/koalas', koalaRouter)
-//#
+// app.use('/todo', toDoRouter)
+
 
 // Start up our server
 app.listen(PORT, () => {
